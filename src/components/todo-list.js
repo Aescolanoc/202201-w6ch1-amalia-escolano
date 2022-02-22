f0699b75156a152cac521b0b418104944f35d21f
Mel Escolano <amalia.escolano@gmail.com>
2022-02-22 12:44:02 +0100

    Refactored add/remove to set correct functionality

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as actions from "../redux/todo/action-creator";
 
 export function ToDoList() {
@@ -10,13 +10,16 @@ export function ToDoList() {
   const dispatch = useDispatch();
 
   const handleSubmit = (ev) => {
+    setNewTask({ ...newTask, [ev.target.name]: ev.target.value });
     ev.preventDefault();
     dispatch(actions.addTask(newTask));
     setNewTask({});
+    ev.target.reset();
   };
 
   const handleChange = (ev) => {
-    setNewTask({ ...newTask, [ev.target.name]: ev.target.value });
+    ev.preventDefault();
+    setNewTask({ ...newTask, id: parseInt(Math.random() * 1_000_000_000, 10), [ev.target.name]: ev.target.value });
   };
 
   return (
@@ -36,8 +39,9 @@ export function ToDoList() {
             <>
               <li key={index}>{task.name}</li>
               <button
+                key={task.name}
                 onClick={() => {
-                  dispatch(actions.removeTask(task.name));
+                  dispatch(actions.removeTask(task));
                 }}
               >
                 delete
--- src/redux/todo/todo-reducer.js
+++ src/redux/todo/todo-reducer.js
@@ -1,13 +1,19 @@
 import { actionTypes } from "./action-types";
 
-const initialState = { tasks: [{ name: "dormir" }, { name: "comer" }] };
+const initialState = {
+  tasks: [],
+};
 
 export function taskReducer(state = initialState, action) {
   switch (action.type) {
+    case actionTypes.load:
+      return { ...state, tasks: action.payload };
     case actionTypes.add:
-      return { tasks: [...state.tasks, action] };
+      return { ...state, tasks: [...state.tasks, action.payload] };
     case actionTypes.remove:
-      return state.tasks.filter((item) => item.name !== action);
+      return { ...state, tasks: state.tasks.filter((item) => item.id !== action.payload.id) };
+    case actionTypes.update:
+      return { ...state, tasks: state.tasks.map((item) => (item.id === action.payload.id ? action.payload : item)) };
     default:
       return state;
   }
